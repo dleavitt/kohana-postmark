046eb37da79792aa1370430b50de0ebbb2bf82eb
Daniel Leavitt <daniel.leavitt@gmail.com>
2012-06-01 20:16:25 -0700

    updates for kohana 3.2

--- classes/kohana/postmark/adapter.php
+++ classes/kohana/postmark/adapter.php
@@ -4,17 +4,17 @@ class Kohana_Postmark_Adapter implements Mail_Postmark_Adapter_Interface {
 	
 	public static function getApiKey()
 	{
-		return Kohana::config('postmark.api_key');
+		return Kohana::$config->load('postmark.api_key');
 	}
 	
 	public static function setupDefaults(Mail_Postmark &$mail)
 	{
-		$mail->from(Kohana::config('postmark.from_address'), Kohana::config('postmark.from_name'));
+		$mail->from(Kohana::$config->load('postmark.from_address'), Kohana::$config->load('postmark.from_name'));
 	}
 	
 	public static function log($logData)
 	{
-		Kohana::$log->add(Kohana::INFO, Arr::get($logData, 'return'));
+    // Kohana::$log->add(Log::INFO, Arr::get($logData, 'return'));
 	}
 }
 
